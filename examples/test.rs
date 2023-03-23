a4a1c2f2be4b32ee61882cea840d25aae84fcc7a
nathan <nathanpinheiro99@gmail.com>
2023-03-23 03:14:40 -0300

    add element read and edit functions

--- examples/test.rs
+++ examples/test.rs
@@ -1,3 +1,6 @@
+use std::borrow::BorrowMut;
+use std::ops::{Deref, DerefMut};
+use std::rc::Rc;
 use macroquad::prelude::*;
 use Simples_menu::{Button, Menu, Text_label};
 
@@ -5,12 +8,19 @@ use Simples_menu::{Button, Menu, Text_label};
 async fn main() {
     let mut menu = Menu::new("Menu".to_string(), Vec2{ x: 50.0, y: 50.0 });
     //menu.size = Some(Vec2 { x: 220.0, y: 80.0 });
-    menu.elements.push(Box::new(Button::new("Buttom 1".to_string(), Vec2{ x: 0.0, y: 0.0 }, None)));
-    menu.elements.push(Box::new(Button::new("Buttom 2".to_string(), Vec2{ x: 0.0, y: 20.0 }, None)));
-    menu.elements.push(Box::new(Text_label::new("Teste de legenda: ".to_string(), Vec2{ x: 30.0, y: 50.0 })));
+
+    //menu.elements.push(Rc::new(Button::new("Buttom 1".to_string(), Vec2{ x: 0.0, y: 0.0 }, None)));
+    //menu.elements.push(Rc::new(Button::new("Buttom 2".to_string(), Vec2{ x: 0.0, y: 20.0 }, None)));
+    //menu.elements.push(Rc::new(Text_label::new("Teste de legenda: ".to_string(), Vec2{ x: 30.0, y: 50.0 })));
+
+
+    let buttan1 = menu.add_element(Button::new("Buttom 1".to_string(), Vec2{ x: 0.0, y: 0.0 }, None));
+    let label1 = menu.add_element(Text_label::new("Teste de legenda: ".to_string(), Vec2{ x: 30.0, y: 50.0 }));
     loop {
         menu.draw();
 
+        label1.edit().title.name = get_fps().to_string();
+        buttan1.edit().position.x += 0.1;
 
         next_frame().await
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,44 @@
+use std::borrow::{Borrow, BorrowMut};
+use std::cell::{Ref, RefCell, RefMut};
+use std::ops::Deref;
+use std::rc::Rc;
 use macroquad::prelude::*;
 use macros::*;
 
-pub trait Drawable {
-    fn draw(&self, start_position: Vec2);
+pub trait MenuElement {
+    fn update(&self, menu_position: Vec2);
+    fn draw(&self, menu_position: Vec2);
     fn bounding_rect(&self) -> Option<Rect>;
 }
 
 pub struct Title {
-    name: String,
-    color: Color,
-    font_size: f32
+    pub name: String,
+    pub color: Color,
+    pub font_size: f32
 }
 impl Title {
+    ///Return the size needed to draw this ´Title´ on the screen.
     fn size(&self) -> Vec2 {
         let text_size = measure_text(&self.name, None, self.font_size as u16, 1.0);
         Vec2{
             x: text_size.width,
             y: text_size.height,
         }
+    }
+}
+
+pub struct Element<T> {
+    id: usize,
+    data: Rc<RefCell<T>>,
+}
+impl<T> Element<T> {
+    ///Return a immutable reference to the element.
+    pub fn read(&self) -> Ref<T> {
+        self.data.deref().borrow()
+    }
+    ///Return a mutable reference to the element.
+    pub fn edit(&self) -> RefMut<T> {
+        self.data.deref().borrow_mut()
     }
 }
 
@@ -32,6 +53,7 @@ pub struct Button {
     // other properties specific to buttons
 }
 impl Button {
+    ///Create a new button with the default arguments.
     pub fn new(lable: String, position: Vec2, size: Option<Vec2>) -> Button {
         let label_title = Title {
             name: lable,
@@ -47,7 +69,11 @@ impl Button {
         }
     }
 }
-impl Drawable for Button {
+impl MenuElement for Button {
+    fn update(&self, menu_position: Vec2) {
+        todo!()
+    }
+
     fn draw(&self, start_position: Vec2) {
         let position = self.position + start_position;
 
@@ -79,6 +105,7 @@ pub struct Text_label {
     size: Vec2,
 }
 impl Text_label {
+    ///Create a new text label with the default arguments.
     pub fn new(lable: String, position: Vec2) -> Text_label {
         let label_title = Title {
             name: lable,
@@ -93,7 +120,11 @@ impl Text_label {
         }
     }
 }
-impl Drawable for Text_label {
+impl MenuElement for Text_label {
+    fn update(&self, menu_position: Vec2) {
+        todo!()
+    }
+
     fn draw(&self, start_position: Vec2) {
         let position = self.position + start_position;
 
@@ -109,41 +140,20 @@ impl Drawable for Text_label {
     }
 }
 
-/*
-#[derive(BoundingRect)]
-struct Checkbox {
-    //title: String,
-    visible: bool,
-    position: Vec2,
-    size: Vec2,
-    checked: bool,
-    // other properties specific to checkboxes
-}
-
-impl Drawable for Checkbox {
-    fn draw(&self, menu_position: Vec2) {
-        // implementation for drawing a checkbox
-    }
-    fn bounding_rect(&self) -> Option<Rect>{
-        self.bounding_rect()
-    }
-}
-
-// other element types and their Drawable implementations...
-*/
-
 pub struct Menu {
     pub title: Title,
     pub visible: bool,
     pub color: Color,
+    pub edge: f32,
     pub position: Vec2,
     pub size: Option<Vec2>,
-    pub elements: Vec<Box<dyn Drawable>>,
+    pub elements: Vec<Rc<RefCell<dyn MenuElement>>>,
 
     visible_color: Color,
 }
 
 impl Menu {
+    ///Create a new menu with the default arguments.
     pub fn new(name: String, position: Vec2) -> Menu {
         Menu{
             title: Title {
@@ -153,6 +163,7 @@ impl Menu {
             },
             visible: true,
             color: DARKGRAY,
+            edge: 20.0,
             position: position,
             size: None,
             elements: vec![],
@@ -160,14 +171,20 @@ impl Menu {
             visible_color: DARKGRAY,
         }
     }
+    ///Adds a new menu element to the **menu.elements** vector,
+    ///and return a Element object that can be edited later.
+    pub fn add_element<'a, T: MenuElement + 'static>(&mut self, element: T) ->  Element<T> {
+        let element_ref = Rc::new(RefCell::new(element));
+        self.elements.push(element_ref.clone());
+        let id = self.elements.len();
 
-    pub fn add_element< 'a, T: Drawable + 'static >(&'a mut self, element: T) -> Box<T> {
-        let mut element_ref = Box::new((element));
-        self.elements.push(element_ref);
-
-        element_ref
+        Element {
+            id: id,
+            data: element_ref,
+        }
     }
 
+    ///draw the menu
     pub fn draw(&self) {
         if !self.visible {
             return;
@@ -176,7 +193,7 @@ impl Menu {
         // calculate the bounding rectangle for the menu
         let mut menu_rect = Rect::new(0.0, 0.0, 0.0, 0.0);
         for element in &self.elements {
-            if let Some(rect) = element.bounding_rect() {
+            if let Some(rect) = element.deref().borrow().bounding_rect() {
                 menu_rect = menu_rect.combine_with(rect);
             }
         }
@@ -205,10 +222,10 @@ impl Menu {
         };
 
         let menu_bg_rect = Rect{
-            x: menu_rect.x - 20.0,
-            y: menu_rect.y - 20.0,
-            w: menu_rect.w + 40.0,
-            h: menu_rect.h + 40.0 + menu_title_rect.h,
+            x: menu_rect.x - self.edge,
+            y: menu_rect.y - self.edge,
+            w: menu_rect.w + (self.edge * 2.0),
+            h: menu_rect.h + (self.edge * 2.0) + menu_title_rect.h,
         };
 
         // draw the menu background
@@ -220,25 +237,7 @@ impl Menu {
 
         // draw the menu elements
         for element in &self.elements {
-            element.draw(Vec2{ x: menu_rect.x, y: menu_rect.y + menu_title_rect.h});
+            element.deref().borrow().draw(Vec2{ x: menu_rect.x, y: menu_rect.y + menu_title_rect.h});
         }
     }
 }
-
-/*
-pub struct MenuManager {
-    menus: HashMap<String , Menu>
-}
-
-impl MenuManager {
-    pub fn add_menu(& mut self, menu: Menu) {
-        self.menus.insert(&menu.title.name, menu)
-    }
-
-    pub fn draw(&self) {
-        for menu in &self.menus {
-            menu.draw();
-        }
-    }
-}
-*/
